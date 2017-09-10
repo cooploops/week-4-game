9dec21c28c8e4838e161e48713bfb8d48da929c3
TJ <cooploops@gmail.com>
2017-09-09 22:59:04 -0700

    fixed atk buttons and spacing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,14 @@ window.onload = function() {
         // create audio files
         var pokemonIntro = document.createElement("audio");
         pokemonIntro.setAttribute("src", "assets/sounds/101-opening.mp3")
+        pokemonIntro.loop = true;
         pokemonIntro.volume = 0.2;
-        // pokemonIntro.play();
+        pokemonIntro.play();
+
+        var pokeBattle = document.createElement("audio");
+        pokeBattle.setAttribute("src","assets/sounds/115-battle-vs-trainer-.mp3")
+        pokeBattle.loop = true;
+        pokeBattle.voluem = 0.2;
 
         var game = {
 
@@ -60,22 +66,69 @@ window.onload = function() {
             },
 
             pokemonSelect: function() {
+                pokemonIntro.pause();
+                // pokeBattle.play();
                 if(this.userChoice === "charmanderStart") {
+                    // setup pokemon
                     $("#pokemon2Img").attr("src","assets/images/charmanderBack.gif");
                     $("#pokemon1Img").attr("src","assets/images/squirtle-3.gif");
-                    var moveList = $("<div>");
-                    moveList.attr("class","moveList pt-5");
-                    $(".container").append(moveList);
-                    moveList.html("<button id='atk1' class='mx-auto'>Scratch</button> <br> <button id='atk2' class='mx-auto'>Flamethrower</<button> <br> <button id='atk3' class='mx-auto'>Flameburst</button> <br> <button id='atk4' class='mx-auto'>Inferno</button>")
 
+                    // setup healthbars
+                    $("#poke2HP").attr("class","progress-bar bg-success");
+                    $("#poke2HP").attr("aria-valuemax",this.charmander.HP);
+                    $("#poke2HP").attr("aria-valuenow",this.charmander.HP);
+                    $("#poke2HP").css("width","100%");
+                    $("#poke2HP").text(this.charmander.HP);
+
+                    $("#poke1HP").attr("class","progress-bar bg-success");
+                    $("#poke1HP").attr("aria-valuemax",this.squirtle.HP);
+                    $("#poke1HP").attr("aria-valuenow",this.squirtle.HP);
+                    $("#poke1HP").css("width","100%");
+                    $("#poke1HP").text(this.squirtle.HP);
+
+                    // setup moves
+                    var moveList = $("<div>");
+                    moveList.attr("class","moveList");
+                    moveList.addClass("moveList");
+                    $(".container").append(moveList);
+                    moveList.html("<div class='row firstRow'><button id='atk1' class='btn btn-lg'>Scratch</button> <button id='atk2' class='btn btn-lg'>Flamethrower</button></div> <div class='row secondRow'><button id='atk3' class='btn btn-lg'>Flameburst</button> <button id='atk4' class='btn btn-lg'>Inferno</button></div>")
+                   
+                    // set stage
                     this.setStage();
                 } else if (this.userChoice === "squirtleStart") {
+                    // setup pokemon
                     $("#pokemon2Img").attr("src","assets/images/squirtleBack.gif");
                     $("#pokemon1Img").attr("src","assets/images/bulbasaur-3.gif")
+                    // setup healthbars
+                    $("#poke2HP").attr("class","progress-bar bg-success");
+                    $("#poke2HP").attr("aria-valuemax",this.squirtle.HP);
+                    $("#poke2HP").attr("aria-valuenow",this.squirtle.HP);
+                    $("#poke2HP").css("width","100%");
+                    $("#poke2HP").text(this.squirtle.HP);
+
+                    $("#poke1HP").attr("class","progress-bar bg-success");
+                    $("#poke1HP").attr("aria-valuemax",this.bulbasaur.HP);
+                    $("#poke1HP").attr("aria-valuenow",this.bulbasaur.HP);
+                    $("#poke1HP").css("width","100%");
+                    $("#poke1HP").text(this.bulbasaur.HP);
                     this.setStage();
                 } else if (this.userChoice === "bulbasaurStart") {
+                    // setup pokemon
                     $("#pokemon2Img").attr("src","assets/images/bulbasaurBack.gif");
                     $("#pokemon1Img").attr("src","assets/images/charmander-3.gif");
+                    // setup healthbars
+                    $("#poke2HP").attr("class","progress-bar bg-success");
+                    $("#poke2HP").attr("aria-valuemax",this.bulbasaur.HP);
+                    $("#poke2HP").attr("aria-valuenow",this.bulbasaur.HP);
+                    $("#poke2HP").css("width","100%");
+                    $("#poke2HP").text(this.bulbasaur.HP);
+
+                    $("#poke1HP").attr("class","progress-bar bg-success");
+                    $("#poke1HP").attr("aria-valuemax",this.charmander.HP);
+                    $("#poke1HP").attr("aria-valuenow",this.charmander.HP);
+                    $("#poke1HP").css("width","100%");
+                    $("#poke1HP").text(this.charmander.HP);
+
                     this.setStage();
                 };
             }
